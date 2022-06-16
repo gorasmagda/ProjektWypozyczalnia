c7c38898d686f48f84207d100a3e571898430193
Aleksandra1500 <Alex_schoepe@wp.pl>
2022-06-16 22:53:07 +0200

    LogInViewModel w ogóle się pojawił, a w nim tylko zarys przycisku "zaloguj"

---
+++ WypozyczalaniaProjekt/ViewModel/LogInViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WypozyczalaniaProjekt.ViewModel.BaseClassess;
+
+namespace WypozyczalaniaProjekt.ViewModel
+{
+    class LogInViewModel
+    {
+        private ICommand zalogujClick;
+
+        public ICommand ZalogujClick => zalogujClick ?? (zalogujClick = new RelayCommand(
+            o => Console.WriteLine("Zadziało się"), null));
+    }
+}
