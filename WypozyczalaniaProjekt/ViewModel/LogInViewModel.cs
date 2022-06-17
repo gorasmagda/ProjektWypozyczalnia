a178a50ccd39ae748e20942da91bcc46dbf405d4
Aleksandra1500 <Alex_schoepe@wp.pl>
2022-06-17 11:47:09 +0200

    Update logowania - jeszcze nie działa ale już prawie (chyba)

--- WypozyczalaniaProjekt/ViewModel/LogInViewModel.cs
+++ WypozyczalaniaProjekt/ViewModel/LogInViewModel.cs
@@ -1,18 +1,95 @@
 ﻿using System;
-using System.Windows.Input;
-using WypozyczalaniaProjekt.ViewModel.BaseClassess;
 
 namespace WypozyczalaniaProjekt.ViewModel
 {
+    using BaseClassess;
+    using System.Collections.ObjectModel;
+    using System.Windows;
+    using System.Windows.Input;
+    using WypozyczalaniaProjekt.DAL.Encje;
+    using WypozyczalaniaProjekt.Model;
+
     class LogInViewModel : ViewModelBase
     {
+        #region Składowe prywatne
+
+        private Model model = null;
+
+        #endregion
+
+        #region Konstruktory
+
+        public LogInViewModel(Model model)
+        {
+            Pracownicy = new ObservableCollection<Pracownik>();
+            this.model = model;
+            Pracownicy = model.Pracownicy;
+        }
+
+        public LogInViewModel(){ } //Czemu bez tego widok nie działa?
+
+        public ObservableCollection<Pracownik> Pracownicy { get; set; }
+
+        #endregion
+
+        private string login;
+        public string Login
+        {
+            get => login;
+            set
+            {
+                login = value;
+                onPropertyChanged(nameof(Login));
+            }
+        }
+
+        private string haslo;
+        public string Haslo
+        {
+            get => haslo;
+            set
+            {
+                haslo = value;
+                onPropertyChanged(nameof(Haslo));
+            }
+        }
+
+        private ICommand wyczyscClick;
+        public ICommand WyczyscClick => wyczyscClick ?? (wyczyscClick = new RelayCommand(
+            o =>
+            {
+                Login = "";
+                Haslo = "";
+            },
+            null));
 
         private ICommand zalogujClick;
         public ICommand ZalogujClick => zalogujClick ?? (zalogujClick = new RelayCommand(
             o =>
             {
-                Console.WriteLine("Zadziało się");
+                if (CzyJestKtosTakiJestWBazie())
+                {
+                    Console.WriteLine("Udalo sie");
+                }
+                else
+                {
+                    MessageBox.Show("Niepoprawny login lub hasło!");
+                }
             },
-            null));
+            o => Login != null && Haslo != null && Login != "" && Haslo != ""));
+
+        public bool CzyJestKtosTakiJestWBazie()
+        {
+            bool wynik = false;
+
+            foreach (var l in model.Pracownicy)
+            {
+                if (Login == l.Email && Haslo == l.Pesel)
+                {
+                    wynik = true;
+                }
+            }
+            return wynik;
+        }
     }
 }
